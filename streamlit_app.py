568984c9c97ad7d0fe250c7c77d3a5f00e36082c
heldielclassico <113118436+heldielclassico@users.noreply.github.com>
2025-12-20 20:28:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ def generate_response(user_input):
         return
 
     model = ChatGoogleGenerativeAI(
-        model="gemini-1.5-flash", 
+        model="gemini-2.5-flash", 
         google_api_key=api_key,
         temperature=0.0
     )
@@ -93,4 +93,5 @@ with st.form("chat_form", clear_on_submit=False):
 # Footer sederhana
 st.markdown("---")
 st.caption("Sumber data: poltesa.ac.id & Quipper Campus")
+
 
